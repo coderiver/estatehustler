12e2695549b370280a148aace0274cc08a993182
whats0n <mrdvv@mail.ru>
2015-12-21 17:54:40 +0200

    update

--- build/js/common.js
+++ build/js/common.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
 
-	// go js
+	$('.js-popup-btn').click(function() {
+		var btn = $(this).data('btn'),
+			popup = $('.js-popup[data-popup="' + btn + '"]'),
+			popups = $('.js-popup');
+		popups.removeClass('is-active');
+		popup.addClass('is-active');
+		return false;
+	});
+
+	$('.js-popup').each(function() {
+		$(this).click(function() {
+			$(this).removeClass('is-active'); 
+		});		
+		$('.js-popup-inner').click(function(event) {
+			event.stopPropagation();	
+		});
+	});
 
 });
