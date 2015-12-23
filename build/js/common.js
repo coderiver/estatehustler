e3bd20088d21c8101e2d4d6b2562a6f8acf9f65e
whats0n <mrdvv@mail.ru>
2015-12-23 13:36:22 +0200

    update

--- build/js/common.js
+++ build/js/common.js
@@ -17,5 +17,7 @@ $(document).ready(function() {
 			event.stopPropagation();	
 		});
 	});
+	
+	$('.js-validation').validator();
 
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
 //=include lib/jquery.js
 //=include lib/slick.min.js
+//=include lib/bootstrap.min.js
+//=include lib/validator.min.js
 //=include common.js
